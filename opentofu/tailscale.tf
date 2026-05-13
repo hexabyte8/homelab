786102a2f148881b87e24b3a976ed010ae84c28e
hexabyte <tparnel96@gmail.com>
2026-05-12 22:46:48 -0500

    updating tailscale acl (#32)

--- opentofu/tailscale.tf
+++ opentofu/tailscale.tf
@@ -49,7 +49,7 @@ resource "tailscale_acl" "as_json" {
           {
             "src": ["autogroup:member"],
             "dst": ["tag:k8s"],
-            "ip":  ["tcp:80", "tcp:443"],
+            "ip":  ["tcp:80", "tcp:443", "tcp:25565", "tcp:25575"],
           },
           {
             "src": ["autogroup:member"],
